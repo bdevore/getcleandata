b945fa89b71348d79813ae06b8f54d17083170e0
Benjamin DeVore <bdevore@123devore.net>
2015-02-17 16:10:26 -0500

    Created R project and setup version control

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+#################################################
+# run_analysis.R - Getting and Cleaning Data course project
+#   Loads multiple data files related to UCI HAR Dataset to
+#   create Tidy data set for analysis purposes
+#
+# Author: Benjamin DeVore
+#
+#################################################
+
+# Load activity labels
+
+# Load feature labels
+
+# For Train Set
+
+## Load Train Data Set
+
+## Only keep mean() and std() values
+
+## Load Train activity labels
+
+## Load Train Subject IDs
+
+## Merge into single data frame
+
+# For Test Set
+
+## Load Test Data Set
+
+## Only keep mean() and std() values
+
+## Load Test activity labels
+
+## Load Test Subject IDs
+
+## Merge into single data frame
+
+# Combine Train and Test data sets
